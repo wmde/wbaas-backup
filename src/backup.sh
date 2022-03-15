11a5fff6ab173b83bffe1424eaa1b8e067dc399d
toan <tobias.andersson@wikimedia.de>
2022-03-15 17:12:18 +0100

    mount manually

--- src/backup.sh
+++ src/backup.sh
@@ -4,11 +4,12 @@
 #DB_PASSWORD=
 #DB_HOST=127.0.0.1
 #DB_PORT=3306
+
+gcsfuse -o nonempty wikibase-dev-sql-backup /mnt/backup-bucket
+
 TIMESTAMP=$(date '+%Y-%m-%d_%H%M%S')
 BACKUP_DIR=/tmp/backup-"$TIMESTAMP"/
 BACKUP_ARCHIVE=/tmp/"mydumper-backup-$TIMESTAMP".tar.gz
-
-rm -rf "$BACKUP_DIR"
 
 # mydumper --user="$DB_USER" \
 #          --port="$DB_PORT" \
@@ -16,7 +17,7 @@ rm -rf "$BACKUP_DIR"
 #          --password="$DB_PASSWORD" \
 #          --outputdir="$BACKUP_DIR"
 
-touch "$BACKUP_DIR"test
+mkdir -p "$BACKUP_DIR"test
 
 tar -cjf "$BACKUP_ARCHIVE" -C "$BACKUP_DIR" .
 
