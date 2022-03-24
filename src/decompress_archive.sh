bc3cb2442932f81ac3b732c499fecb6378716f19
Tobias <tobias.andersson@wikimedia.de>
2022-03-24 08:33:31 +0100

    2. Add hadolint (#3)
    * Add Hadolint
    
    * Fix linting
    
    * 3. Add shellcheck (#4)
    
    * Add shellcheck
    
    * Fix linting

--- src/decompress_archive.sh
+++ src/decompress_archive.sh
@@ -2,5 +2,5 @@
 set -e
 
 # example: BACKUP_KEY=asdf ./decompress_archive.sh /backups/mydumper-backup-2022-03-18_122503.tar.gz /tmp/outdir
-mkdir -p $2
-openssl enc -pass "pass:$BACKUP_KEY" -pbkdf2 -d -aes256 -in "$1" | tar xz -C $2
+mkdir -p "$2"
+openssl enc -pass "pass:$BACKUP_KEY" -pbkdf2 -d -aes256 -in "$1" | tar xz -C "$2"
