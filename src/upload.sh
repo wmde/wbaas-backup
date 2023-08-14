ced3263e921c3a60d24413ed05c7869d083ede97
Frederik Ring <frederik.ring@wikimedia.de>
2023-08-14 13:49:42 +0200

    fix: copy directory contents instead of directory

--- src/upload.sh
+++ src/upload.sh
@@ -2,4 +2,4 @@
 
 set -e
 mc alias set remote "$STORAGE_ENDPOINT" "$STORAGE_ACCESS_KEY" "$STORAGE_SECRET_KEY" --api "$STORAGE_SIGNATURE_VERSION"
-mc cp --recursive /backups/output remote/"$STORAGE_BUCKET_NAME"
+mc cp --recursive /backups/output/ remote/"$STORAGE_BUCKET_NAME"
